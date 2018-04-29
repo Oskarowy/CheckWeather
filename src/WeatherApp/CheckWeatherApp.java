3f0372eaebeab742a6a7557b1c582c129f6b12f6
Oski <oskar.bialek.programista@gmail.com>
2018-04-29 20:18:27 +0200

    Initial commit

---
+++ src/WeatherApp/CheckWeatherApp.java
@@ -0,0 +1,25 @@
+package WeatherApp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class CheckWeatherApp extends Application{
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        WeatherPaneOrganizer main = new WeatherPaneOrganizer();
+        stage.setTitle("Check weather");
+        stage.setResizable(false);
+
+        Scene root = new Scene(main.getRoot());
+        stage.setScene(root);
+        stage.show();
+    }
+}
